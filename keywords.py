5511de617d2cf0840cb9d50b65590ae2b80fb658
zero <151121577+zerorzr@users.noreply.github.com>
2025-10-03 20:08:00 +0300

    Create keywords.py

---
+++ keywords.py
@@ -0,0 +1,750 @@
+hentai
+kys
+kill your self
+kï¿¶ï¿¶ï¿¶yï¿¶ï¿¶s
+pussie
+pusy
+pencils
+aids
+anus
+ass
+asses
+asshole
+assholes
+asskisser
+asswipe
+balls
+bastard
+basterd
+beastial
+beastiality
+beastility
+bestial
+bestiality
+biatch
+bich
+bitch
+bitcher
+bitchers
+bitches
+bitchin
+bitching
+blow
+blowjob
+blowjobs
+bonehead
+boner
+bullshit
+bum
+bunghole
+butt
+buttbreath
+buttfucker
+butthair
+buttface
+buttfuck
+buttfucker
+butthead
+butthole
+buttpicker
+chink
+circlejerk
+clit
+cobia
+cock
+cocks
+cocksuck
+cocksucked
+cocksucker
+cocksucking
+cocksucks
+cok
+cooter
+crap
+cum
+cummer
+cumming
+cums
+cumshot
+cunilingus
+cunillingus
+cunnilingus
+cunt
+cuntlick
+cuntlicker
+cuntlicking
+cunts
+cyberfuc
+cyberfuck
+cyberfucked
+cyberfucker
+cyberfuckers
+cyberfucking
+dam
+dick
+dike
+dildo
+dildos
+dipshit
+douche
+douchebag
+dumass
+dumbass
+dyke
+ejaculate
+ejaculated
+ejaculates
+ejaculating
+ejaculatings
+ejaculation
+fag
+faget
+fagg
+fagget
+fagging
+faggit
+faggot
+faggs
+fagit
+fagot
+fagots
+fags
+fart
+farted
+farting
+fartings
+farts
+farty
+fatass
+fatso
+felatio
+fellatio
+fingerfuck
+fingerfucked
+fingerfucker
+fingerfuckers
+fingerfucking
+fingerfucks
+fistfuck
+fistfucked
+fistfucker
+fistfuckers
+fistfucking
+fistfuckings
+fistfucks
+focker
+fuck
+fucked
+fucker
+fuckers
+fuckin
+fucking
+fuckings
+fuckme
+fucks
+fuckyou
+fuk
+fuks
+furburger
+fyou
+gangbang
+gangbanged
+gangbangs
+gay
+gaysex
+gazongers
+goddamn
+gonads
+gook
+guinne
+hardon
+hardcoresex
+hoe
+homo
+hooker
+horniest
+horny
+hotsex
+hump
+hussy
+jackoff
+jackass
+jackingoff
+jackoff
+jack-off
+jap
+jerk
+jerk-off
+jew
+jism
+jiz
+jizm
+jizz
+kike
+kkk
+kock
+kondum
+kondums
+kraut
+kum
+kummer
+kumming
+kums
+kunilingus
+lesbian
+lesbo
+lust
+lusting
+mayonnaise
+merde
+molest
+mothafuck
+mothafucka
+mothafuckas
+mothafuckaz
+mothafucked
+mothafucker
+mothafuckers
+mothafuckin
+mothafucking
+mothafuckings
+mothafucks
+motherfuck
+motherfucked
+motherfucker
+motherfuckers
+motherfuckin
+motherfucking
+motherfuckings
+motherfucks
+muff
+nazi
+n@zi
+n@z!
+n@z1
+naz1
+naz!
+nerd
+nigga
+niggas
+nigger
+niggers
+omfg
+orgasim
+orgasims
+orgasm
+orgasms
+pecker
+penis
+phonesex
+phuk
+phuked
+phuking
+phukked
+phukking
+phuks
+phuq
+pimp
+piss
+pissed
+pisser
+pissers
+pisses
+pissin
+pissing
+pissoff
+porn
+porno
+pornography
+pornos
+prick
+pricks
+prostitute
+punk
+pussies
+pussy
+pussys
+queer
+rape
+retard
+rtb
+schlong
+sheister
+sh1t
+shit
+shited
+shitfull
+shiting
+shitings
+shits
+shitte
+shitted
+shitter
+shitters
+shitting
+shittings
+shitty
+sex
+sexx
+sexxx
+sexy
+sleaze
+slut
+sluts
+smut
+snatch
+spunk
+stfu
+suck
+suk
+tbm
+thong
+toten
+tramp
+tranny
+twat
+vibrator
+wang
+wank
+wanker
+wetback
+whore
+wop
+wtf
+ȂƧƧ
+ȂƧƧɧȪŁĒ
+Ƈüčĸ
+ĠǺÝ
+śҼҳ
+ŞʜʝȽ
+ƭǺğğŐŦ
+ϑǺȠϑ ßǺňϑ
+ƤȩᵈϘ
+Ѣɖṧᵯ
+ɍʋĈЌ
+ɍΕτЇȿȟ
+kink
+ȰɌǤǺʂɱ
+ȰɌǤʏ
+ɮǺŞƬǺƦɗ
+ǺÑǺȴ
+˅ǺğЇɴǺ
+ᵖƐƞɉş
+ǷɄϨϨȳ
+ƧǕƈҚ
+dȋϾҡ
+ΗɪƮȴƐŔ
+ḉɄϻ
+ẃḨỠẛỄ
+Đįśϛοʀɖ
+ĎǺԁԁϒ
+Ӎӧɭȩšƚ
+Ӎӧɭȩšƚȅɼ
+ɾȅȶǟɽd
+cun†
+†hot
+†wat
+ҠҠҠ
+Ҡӏǖ Ҡӏǖx Ҡӏǻn
+ńǻzї
+isis
+Ǻ˪˪ahu Ǻkßar
+ĸƴs
+ĸì˪˪ yourself
+†it
+ȶȋƚƚy
+ȶȋƚƚȋes
+swastika
+Osama B¡nIaden
+Ļɐɯd
+ǹǜdȇ
+ǹǜdȇs
+ŝǘϛϛ
+Αβόгtiοn
+sƬƒʋ
+sta˪ker
+śҼҳt
+shǻg
+wǻnǩ
+wǻnǩȅr
+ȇɾecɟ
+ȇɾecɟion
+ƁƖȏɯjob
+Ҏіss
+ͼόϛκ
+ᵖƐƞɉle dysfunction
+passɯord
+ǻddress
+ǻnus
+ǻѕѕΗat
+ǻѕѕ˪icker
+ßооβȊέs
+ßооtycall
+ßɾɛaᵴȽ
+ßѻǹg
+ɯeed
+ʍeȽȟ
+dȋϾҡrider
+ɍʋĈЌer
+Mother ɍʋĈЌer
+βǻ˪˪ sАϲκ
+βǻƞģ me
+Gangβǻƞģ
+Ǻʂʂmuncher
+ßëǻstiality
+breastjob
+boobjob
+cǻґҎet muncher
+dȋϾҡ cheese
+dȋϾҡ muncher
+сϋиt muncher
+ƒƦȇȇ ŔŐßÛX
+Ͼٌِِ ӧӣԁَّؒ ϘМ
+bumɍʋĈЌ
+Ǻtivan
+ȻƦɣsƬal ʍeȽȟ
+įβύрrОҒen
+Ǻᴅᴅerall
+Ǻlρrazolam
+Ǻmoxicillin
+Ͼǻnnabinoids
+ɱariujana
+Blunt
+gǻnja
+3oo3s
+l3oo3s
+l}IC|{
+givemesomeheadplease
+uglyass
+cum cum cumin
+crock and ball torture
+cum;o
+blackass
+slowass
+badass
+🅟u$$yAss
+DumAss
+$h¡t
+RETARD
+retard
+Seっっx
+Sevex
+Bixitxtch
+Biっっtcっっh
+丂乇乂
+几丨ᎶᎶ卂
+几丨ᎶᎶ乇尺
+几丨ᎶᎶ卂丂
+几丨ᎶᎶ乇尺丂
+千卂ᎶᎶㄖㄒ
+千卂ᎶᎶㄖㄒ丂
+乃乇卂几乇尺
+乃乇卂几乇尺丂
+匚ㄩ爪
+尺乇ㄒ卂尺ᗪ
+尺乇ㄒ卂尺ᗪ丂
+尺乇ㄒ卂尺ᗪ乇ᗪ
+丂卄丨ㄒ
+千ㄩ匚Ҝ
+千ㄩ匚Ҝ丨几Ꮆ
+千ㄩ匚Ҝ乇尺
+千ㄩ匚Ҝ丂
+千ㄩ匚Ҝ乇尺丂
+爪ㄖㄒ卄乇尺千ㄩ匚Ҝ乇尺
+爪ㄖㄒ卄乇尺千ㄩ匚Ҝ乇尺丂
+爪ㄖㄒ卄乇尺千ㄩ匚Ҝ丨几Ꮆ
+匚ㄩ几ㄒ
+卄丨ㄒㄥ乇尺
+卂ᗪㄖㄥ千
+ᐯ卂Ꮆ丨几卂
+卩ㄩ丂丂ㄚ
+ᐯ卂Ꮆ丨几卂丂
+ᐯㄩㄥᐯ卂
+ᐯㄩㄥᐯ卂丂
+乃ㄖㄖ乃
+乃ㄖㄖ乃丂
+乃尺乇卂丂ㄒ
+乃尺乇卂丂ㄒ丂
+ᗪ卂ᗪᗪㄚ
+ᗪ卂爪几
+卄乇ㄥㄥ
+卄ㄖ乇
+卄ㄖ乇丂
+Ꮆ卂ㄚ
+ﾌ乇山
+ﾌ乇山丂
+ﾌ乇山丨丂卄
+ﾌ乇山丨丂卄 卩乇ㄖ卩ㄥ乇
+爪乇ㄒ卄
+匚尺ㄚ丂ㄒ卂ㄥ 爪乇ㄒ卄
+卂ㄒ丨ᐯ卂几
+卩卂丂丂山ㄖ尺ᗪ
+山乇 匚卂几 乃乇 几丨ᎶᎶ卂丂
+ᗪ乇乇乙 几ㄩㄒ丂
+乃卂ㄥㄥ丂
+几ㄩㄒ
+几ㄩㄒ丂
+卂ᗪᗪ尺乇丂丂
+ㄥㄖ尺卂乙乇卩卂爪
+山乇乇ᗪ
+ㄒ山卂ㄒ
+ㄒ卄ㄖㄒ
+ㄒ山ㄖㄒ
+ҜҜҜ
+Ҝㄩ Ҝㄥㄩ乂 Ҝㄥ卂几
+卂丂丂
+卂丂丂乇丂
+卂丂丂卄ㄖㄥ乇
+卂丂丂卄ㄖㄥ乇丂
+乃丨ㄒ匚卄
+乃丨ㄒ匚卄乇丂
+匚ㄩ匚Ҝ
+匚ㄩ匚Ҝ丂
+ㄖ尺Ꮆ卂丂爪
+ㄖ尺Ꮆㄚ
+乃卂丂ㄒ卂尺ᗪ
+乃卂丂ㄒ卂尺ᗪ丂
+卂几卂ㄥ
+卩乇几丨丂
+卩乇几丨丂乇丂
+ᗪ丨匚Ҝ
+ᗪ丨匚Ҝ丂
+卩ㄖ尺几
+卩ㄖ尺几卄ㄩ乃
+乂乂乂
+乂乂乂 卩ㄖ尺几 ᐯ丨ᗪ乇ㄖ
+乂乂乂 卩ㄖ尺几 ᐯ丨ᗪ乇ㄖ丂
+乂几乂乂
+乂 ᐯ丨ᗪ乇ㄖ
+乂 ᐯ丨ᗪ乇ㄖ丂
+乂乂乂 ᐯ丨ᗪ乇ㄖ丂
+丂ㄩ匚匚
+丂ㄩ匚Ҝ
+丂ㄩ匚Ҝ 爪ㄚ ᗪ丨匚Ҝ
+丂ㄩ匚Ҝ 爪ㄚ 卂丂丂
+丂ㄩ匚Ҝ 爪ㄚ ᐯㄩㄥᐯ卂
+丂ㄩ匚Ҝ 爪ㄚ ᐯ卂Ꮆ丨几卂
+丂ㄩ匚Ҝ 爪ㄚ 乃卂ㄥㄥ丂
+ᗪ丨匚
+ᗪ丨Ҝ
+ᗪ丨匚丂
+ᗪ丨Ҝ丂
+ㄒ丨ㄒ
+ㄒ丨ㄒ丂
+ㄒ丨ㄒㄒㄚ
+ㄒ丨ㄒㄒ丨乇丂
+丂ㄒ卂ㄥҜ乇尺
+丂ㄒ千ㄩ
+山ㄒ千
+ﾌ乇丂ㄩ丂
+ﾌ乇丂ㄩ丂 匚卄尺丨丂ㄒ
+ﾌ乇丂ㄩ丂 千ㄩ匚Ҝ丨几Ꮆ 匚卄尺丨丂ㄒ
+山卂几Ҝ
+山卂几Ҝ丂
+山卂几Ҝ乇尺
+山卂几Ҝ乇尺丂
+ㄥ爪卂ㄖ
+ㄥ卂ㄩᎶ卄丨几Ꮆ 爪ㄚ 卂丂丂 ㄖ千千
+乃乃匚
+乃丨Ꮆ 乃ㄥ卂匚Ҝ 匚ㄖ匚Ҝ
+卂几ㄩ丂
+卂几ㄩ丂乇丂
+乃ㄩㄒㄒ
+乃ㄩㄒㄒ丂
+匚ㄖ匚Ҝ
+匚ㄖ匚Ҝ丂
+匚ㄖ匚匚
+匚ㄖҜҜ
+匚ㄖ匚匚丂
+匚ㄖҜҜ丂
+丂ㄩ匚Ҝ 爪ㄚ 匚ㄖ匚Ҝ
+卩乇ᗪㄖ
+卩乇ᗪㄖ丂
+乃ᗪ丂爪
+乇尺乇匚ㄒ丨ㄥ乇 ᗪㄚ丂千ㄩ几匚ㄒ丨ㄖ几
+Ꮆㄖ ㄒㄖ 卄乇ㄥㄥ
+ᗪ尺ㄩᎶ
+ᗪ尺ㄩᎶ丂
+乃ㄖ乃丂
+几丨Ꮆ
+几丨ᎶᎶ
+几ㄩᎶ
+Fujick
+Fat4ss
+s3x
+coems
+Noiggee
+Bastard
+Noiggee
+Noigger
+blackass
+slowass
+badass
+🅟u$$yAs$
+DumAs$
+Ni gaa
+Ni ker
+3oo3s
+l3oo3s
+l}IC|{
+givemesomeheadplease
+uglya$s
+CuMin
+hail hitbler
+niger14
+cxoon
+kxxyxs
+immaSlapYouWithMyDingDo..ng
+lap danze
+jjFvckjj
+jjRetardjj
+jjfvcingjj jjretardjj
+jjNlGGAjj
+|=uk
+/“ussy
+/‘’US$Y
+/-\$s
+Dumb@s$
+Ne gaa
+Ni ker
+N¡ gaa
+Knee guh
+R|\!P@R|\!
+^|^its
+$h¡t
+Ho
+[um
+[raker
+I3eaner
+Beenexr
+Ɓįƚƈƕ
+ȂƧƧ
+ȂƧƧɧȪŁĒ
+Ƈüčĸ
+ŞʜʝȽ
+ƭǺğğŐŦ
+ϑǺȠϑ ßǺňϑ
+ƤȩᵈϘ
+Ѣɖ$ᵯ
+ɍΕτЇȿȟ
+kink
+ȰɌǤǺʂɱ
+ȰɌǤʏ
+ɮǺŞƬǺƦɗ
+ᵖƐƞɉş
+ǷɄϨϨȳ
+ƧǕƈҚ
+Đįśϛοʀɖ
+ĎǺԁԁϒ
+Ӎӧɭȩšƚ
+Ӎӧɭȩšƚȅɼ
+cun†
+†hot
+†wat
+ҠҠҠ
+Ҡӏǖ Ҡӏǖx Ҡӏǻn
+ńǻzї
+isis
+ĸƴs
+ĸì˪˪ yourself
+†it
+ȶȋƚƚy
+ȶȋƚƚȋes
+swastika
+Ļɐɯd
+Αβόг..tiοn
+sƬƒʋ
+sta˪ker
+shǻg
+wǻnǩ
+wǻnǩȅr
+ȇɾecɟ
+Ҏіss
+ᵖƐƞɉle dysfunction
+passɯord
+ǻddress
+ǻnus
+ǻѕѕΗat
+ǻѕѕ˪icker
+ßоо//βȊέs
+ßɾɛaᵴȽ
+ßѻǹg
+ɯeed
+ʍeȽȟ
+dȋϾҡrider
+Mother ɍʋĈЌer
+βǻ˪˪ sАϲκ
+βǻƞģ me
+Gangβǻƞģ
+Ǻʂʂmuncher
+ßëǻstiality
+cǻґҎet muncher
+сϋиt muncher
+ƒƦȇȇ ŔŐßÛX
+Ͼٌِِ ӧӣԁَّؒ ϘМ
+bumɍʋĈЌ
+Ǻtivan
+ȻƦɣsƬal ʍeȽȟ
+įβύрrОҒen
+Ǻᴅᴅerall
+Ǻlρrazolam
+Ǻmoxicillin
+Ͼǻnnabinoids
+ɱariujana
+Blunt
+gǻ..nja
+l}IC
+Biっっtcっっh
+F اا u ااck
+S اا h ااit
+S ااex
+freesex
+fr33s3x
+sh0t
+die
+k1llurself
+k ys
+k y s
+ky s
+s e x
+f u c k
+s t f u
+bad@ss
+@ss
+s@x
+f@ck
+f#ck
+f*ck
+c0ck
+c@ck
+boobs
+b00bs
+b0ob
+b0bs
+bobs
+boob
+b00b
+bo0b
+booob
+buubs
+n8igga
+n 8 i g g a
+n1igga
+n1gga
+pipi
+digga
+suk my
+suk ma
+deek
+deck
+deeck
+hitbler
+fᆞuᆞck?ᆞBᆞLᆞM
+asᆞs
+bᆢitch
+m○an
+givemesomeheadplease
+l3003s
+$#!t
+Ŧυcked
+Dumb@s$
